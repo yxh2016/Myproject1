9a067fe2cfac6f9f986573e4330343acf2ebdd8f
yxh2016 <1246543651@qq.com>
2016-05-29 12:37:47 +0800

    第一个项目

---
+++ src/test.java
@@ -0,0 +1,18 @@
+
+public class test {
+	 public static void main(String[] args)  
+	    {  
+	          
+		 System.out.println("¾Å¾Å³Ë·¨±í");
+	       for(int i=1;i<=9;i++)
+	       {
+	    	   for(int j=1;j<=i;j++)
+	    		   System.out.print(j+"*"+i+"="+i*j+"\t");
+	    	   System.out.println();
+	    	  }
+	      
+	          
+	   
+}
+
+}
